0a8cf1a6192128abd8e352aae7f6e840380bb5fb
grechdanya <grechdania@gmail.com>
2024-08-09 16:56:08 +0200

    Whomp-whomp in program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Enter first client's name");
+string? client_name_1 = Console.ReadLine();
+Console.WriteLine("Enter the first product's name and price");
+string? product_1 = Console.ReadLine();
+Console.WriteLine("Enter first client's adress");
+string? adress_1 = Console.ReadLine();
+
+Console.WriteLine("Enter second client's name");
+string? client_name_2 = Console.ReadLine();
+Console.WriteLine("Enter the second product's name and price");
+string? product_2 = Console.ReadLine();
+Console.WriteLine("Enter second client's adress");
+string? adress_2 = Console.ReadLine();
+
+Console.WriteLine("Enter third client's name");
+string? client_name_3 = Console.ReadLine();
+Console.WriteLine("Enter the third product's name and price");
+string? product_3 = Console.ReadLine();
+Console.WriteLine("Enter third client's adress");
+string? adress_3 = Console.ReadLine();
+
+Console.WriteLine($"Ordere No 1:");
+Console.WriteLine($"Name: {client_name_1}");
+Console.WriteLine($"Product: {product_1}");
+Console.WriteLine($"Adress: {adress_1}");
+
+Console.WriteLine($"\nOrdere No 2:");
+Console.WriteLine($"Name: {client_name_2}");
+Console.WriteLine($"Product: {product_2}");
+Console.WriteLine($"Adress: {adress_2}");
+
+Console.WriteLine($"\nOrdere No 3:");
+Console.WriteLine($"Name: {client_name_3}");
+Console.WriteLine($"Product: {product_3}");
+Console.WriteLine($"Adress: {adress_3}");
